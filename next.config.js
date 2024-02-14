a589d576cdc17759d121946c71426ca82968f611
Bruno Matheus <106699454+brunoedif@users.noreply.github.com>
2024-02-14 02:58:51 -0300

    up06

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     reactStrictMode: true,
     images: {
-        domains: ['superpao.online'],
+        domains: ['app.superpao.online'],
         staticPageGenerationTimeout: 1500, //Domain of image host
     },
     
