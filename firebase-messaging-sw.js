d1a111ef27c82f5dc0a8624941474c9a74f91ec9
Bruno Matheus <106699454+brunoedif@users.noreply.github.com>
2024-02-13 21:13:09 -0300

    up12

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,18 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyAAloJqungqqBa0di6yDD_hL2E6Qi4IcVs",
+    authDomain: "vital-octagon-215300.firebaseapp.com",
+    databaseURL: "https://vital-octagon-215300.firebaseio.com",
+    projectId: "vital-octagon-215300",
+    storageBucket: "vital-octagon-215300.appspot.com",
+    messagingSenderId: "657849172002",
+    appId: "1:657849172002:web:0da82fc1e9cd847c14ca7b"
+  };
 
-    // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-    import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js'
+const app = initializeApp(firebaseConfig);
 
-    // Add Firebase products that you want to use
-    import { getAuth } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js'
-    import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
-
-firebase.initializeApp({
-     apiKey: "AIzaSyAAloJqungqqBa0di6yDD_hL2E6Qi4IcVs",
-      authDomain: "vital-octagon-215300.firebaseapp.com",
-      databaseURL: "https://vital-octagon-215300.firebaseio.com",
-      projectId: "vital-octagon-215300",
-      storageBucket: "vital-octagon-215300.appspot.com",
-      messagingSenderId: "657849172002",
-      appId: "1:657849172002:web:0da82fc1e9cd847c14ca7b"
-});
-
-const messaging = firebase.messaging();
+const messaging = getMessaging(app)
 
 messaging.setBackgroundMessageHandler(function (payload) {
     const promiseChain = clients
